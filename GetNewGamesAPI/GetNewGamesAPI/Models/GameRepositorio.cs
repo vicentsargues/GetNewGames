3af05c2c9ba202f598d7d9f790f9dfc00e9e52b2
vicentsargues <vicentsargues@gmail.com>
2021-06-10 16:17:53 +0200

    was

--- GetNewGamesAPI/GetNewGamesAPI/Models/GameRepositorio.cs
+++ GetNewGamesAPI/GetNewGamesAPI/Models/GameRepositorio.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using Placemybet.Models;
 
 namespace GetNewGamesAPI.Models
 {
--- GetNewGamesAPI/GetNewGamesAPI/Models/SaleRepositorio.cs
+++ GetNewGamesAPI/GetNewGamesAPI/Models/SaleRepositorio.cs
@@ -1,5 +1,4 @@
 ﻿using MySql.Data.MySqlClient;
-using Placemybet.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
