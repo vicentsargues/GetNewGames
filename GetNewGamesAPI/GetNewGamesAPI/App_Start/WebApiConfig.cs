5dfc8f619e3dc5d8319ff9369e320b4b71dd96b4
vicentsargues <vicentsargues@gmail.com>
2021-06-10 18:37:01 +0200

    uwu

--- GetNewGamesAPI/GetNewGamesAPI/App_Start/WebApiConfig.cs
+++ GetNewGamesAPI/GetNewGamesAPI/App_Start/WebApiConfig.cs
@@ -19,6 +19,14 @@ namespace GetNewGamesAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+
+            //Evito las referencias circulares al trabajar con Entity FrameWork         
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Serialize;
+            config.Formatters.JsonFormatter.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+
+            //Elimino que el sistema devuelva en XML, sólo trabajaremos con JSON
+            config.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
         }
     }
 }
